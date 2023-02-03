cf5e32fd4442364dec4474664975878f8d62c503
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-02-02 23:07:53 -0700

    added function in script file to store user input data from the text area. added function in script file to recall user input data. added event listener for the save button click. updated html file to adjust the time blocks classes and id's. checked all of the functions and the text area stores user input after clicking the save button. webpage retrieves stored input from local storage after refreshing the page and when the page is closed and reopened.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,48 @@
 console.log(window);
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//ensures that the code isn't run until the browser has finished rendering all the elements in the html
+window.onload = function() {
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+ 
+  var output_div = document.querySelector(".col-8.col-md-10.description");
+  var save_buttons = document.querySelectorAll("#saveButton");
+  var textareas = document.querySelectorAll("textarea")
+  var rightNow = new Date().getHours()
+
+  // current time
+  console.log(rightNow)
+
+// event listener
+  save_buttons.forEach(btn => {
+    btn.addEventListener("click", updateOutput);
+  })
+
+  //recall local storage 
+  textareas.forEach(textarea => {
+    textarea.value = localStorage.getItem(textarea.id);
+  })
+
+  //input for local storage 
+  function updateOutput(e) {
+    var input_textarea = e.target.previousElementSibling;
+    console.log("HERE", e.target.previousElementSibling)
+    console.log(input_textarea.value)
+    localStorage.setItem(e.currentTarget.dataset.hour, input_textarea.value);
+
+    output_div.textContent = input_textarea.value;
+  }
 
 //current date and time 
   const d = new Date();
   document.getElementById("currentDay").innerHTML = d;
 
 });
+};
