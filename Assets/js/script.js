d6c50fc362bd59ba347979ae534540d8d5258128
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-02-03 02:25:08 -0700

    added id's to outer divs for styling in javascript. added functions in javascript file to take current time and add color styling to the text areas in the time blocks to classify the differences between past, future and present.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,48 +1,40 @@
-console.log(window);
+$(function (){ 
+  const output_div = document.querySelector(".col-8.col-md-10.description");
+  const save_buttons = document.querySelectorAll("#saveButton");
+  const textareas = document.querySelectorAll("textarea");
+  const timeBlocks = document.querySelectorAll(".time-block");
+  const rightNow = new Date().getHours();
 
-//ensures that the code isn't run until the browser has finished rendering all the elements in the html
-window.onload = function() {
-
-$(function () {
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
- 
-  var output_div = document.querySelector(".col-8.col-md-10.description");
-  var save_buttons = document.querySelectorAll("#saveButton");
-  var textareas = document.querySelectorAll("textarea")
-  var rightNow = new Date().getHours()
-
-  // current time
-  console.log(rightNow)
-
-// event listener
+  function setColor() {
+    timeBlocks.forEach(timeBlock => {
+      if (rightNow > parseInt(timeBlock.id)) {
+        timeBlock.classList.add("past");
+      } else if (rightNow < parseInt(timeBlock.id)) {
+        timeBlock.classList.add("future");
+      } else {
+        timeBlock.classList.add("present");
+      }
+    })
+  }
+  // event listener
   save_buttons.forEach(btn => {
     btn.addEventListener("click", updateOutput);
   })
-
   //recall local storage 
   textareas.forEach(textarea => {
     textarea.value = localStorage.getItem(textarea.id);
   })
-
   //input for local storage 
   function updateOutput(e) {
-    var input_textarea = e.target.previousElementSibling;
-    console.log("HERE", e.target.previousElementSibling)
-    console.log(input_textarea.value)
-    localStorage.setItem(e.currentTarget.dataset.hour, input_textarea.value);
+    const input_textarea = e.target.previousElementSibling;
 
+    localStorage.setItem(e.currentTarget.dataset.hour, input_textarea.value);
     output_div.textContent = input_textarea.value;
   }
-
-//current date and time 
+  //current date and time 
   const d = new Date();
   document.getElementById("currentDay").innerHTML = d;
 
+  // invoke function to set time block styling
+  setColor();
 });
-};
